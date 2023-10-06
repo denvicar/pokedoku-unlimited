08baf31434dda53612e7140d8647de229b4f1dcc
Ciro Faenza <cfaenza7@gmail.com>
2023-10-06 22:59:22 +0200

    Initial commit

---
+++ app/lib/constants.js
@@ -0,0 +1,38 @@
+let types = [
+    'fire',
+    'water',
+    'grass',
+    'thunder',
+    'ice',
+    'poison',
+    'ground',
+    'normal',
+    'rock',
+    'flying',
+    'ghost',
+    'dark',
+    'dragon',
+    'fairy',
+    'fighting',
+    'psychic',
+    'bug',
+]
+let regions = [
+    'Kanto',
+    'Johto',
+    'Hoenn',
+    'Sinnoh',
+    'Unova',
+    'Kalos',
+    'Alola',
+    'Galar',
+    'Paldea',
+]
+let special = [
+    'legendary',
+    'mythical'
+]
+
+const SCHEMA_SIZE = 3
+
+export {types,regions,special, SCHEMA_SIZE}
---
+++ app/lib/gameSchema.js
@@ -0,0 +1,73 @@
+import {special, types, SCHEMA_SIZE, regions} from "@/app/lib/constants";
+
+
+const buildSchema = (pokemons) => {
+    // scelgo tre tipi a caso per le righe
+    // per le prime due colonne filtro i tipi per quelli possibili
+    // poi ne prendo uno a caso
+    let rows = new Set()
+    let availableTypes = new Set(types)
+    console.log('Start, available types', availableTypes)
+    for(let i = 0; i<SCHEMA_SIZE; i++) {
+        let type = getRandomElement([...availableTypes])
+        rows.add(type)
+        availableTypes.delete(type)
+    }
+    console.log("rows are",rows)
+
+    let columns = new Set()
+    let pokemonTypes = simplifyPokemons(pokemons,rows)
+
+    for (let i = 0; i<SCHEMA_SIZE-1; i++) {
+        availableTypes = getAvailableTypesByRows([...rows], pokemonTypes, availableTypes)
+        let type = getRandomElement([...availableTypes])
+        columns.add(type)
+        availableTypes.delete(type)
+    }
+
+    // filtro per regione, non posso fare mitici e leggendari per ora
+    // devo filtrare le regioni per trovare quelle che hanno tutte le righe
+    let availableRegions = getAvailableRegionsByRows([...rows])
+    columns.add(getRandomElement([...availableRegions]))
+    return [[...rows],[...columns]]
+}
+
+const getAvailableTypesByRows = (rows, pokemonTypes, availableTypes) => {
+    let returnTypes = new Set()
+    let pokemonExists = false
+    for (const type of availableTypes) {
+        for (let i = 0; i<rows.length; i++) {
+            for (const pt of pokemonTypes) {
+                if ((pt[0]===type || pt[1]===type) && (pt[0]===rows[i] || pt[1]=== rows[i])) {
+                    pokemonExists = true
+                    break
+                }
+            }
+            if (pokemonExists === false) break
+            pokemonExists = false
+            if (i===rows.length-1) returnTypes.add(type)
+        }
+    }
+    return returnTypes
+}
+
+const getAvailableRegionsByRows = (rows) => {
+    let r = new Set(regions)
+    if (rows.includes('dark')) r.delete('Kanto')
+    r.delete('Hisui')
+    return r
+}
+
+const getRandomElement = (ts) => {
+    let i = Math.floor(Math.random()*ts.length-1)
+    return ts[i]
+}
+
+const simplifyPokemons = (pokemons,rows) => {
+    return Array.from(pokemons.values())
+        .filter(p => p.types.length > 1)
+        .filter(p => rows.has(p.types[0]) || rows.has(p.types[1]))
+        .map(p => p.types)
+}
+
+export {buildSchema}
