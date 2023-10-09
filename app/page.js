c9247f7937e3d55afbb7a94ba47c1205c8f29e37
Ciro Faenza <cfaenza7@gmail.com>
2023-10-09 20:55:56 +0200

    Added new game

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
 import Image from 'next/image'
 import { getDatabase, ref, get, child } from 'firebase/database'
-import app from './lib/db'
+import getData from './lib/db'
 import Schema from "@/app/components/schema";
 import './home.css'
 import {buildSchema} from "@/app/lib/gameSchema";
+import Link from "next/link";
 
 
 export default async function Home() {
@@ -13,15 +14,11 @@ export default async function Home() {
   return (
     <main className="container-fluid">
       <h3>Pokedoku Unlimited</h3>
+      <Link href="/typequiz/">Type quiz </Link>
+      <Link href="/">Classic</Link>
         <Schema pokemons={pokemons} firstSchema={schema} />
     </main>
   )
 }
 
-async function getData() {
 
-  const dbRef = ref(getDatabase(app),"data")
-  const data = await get(child(dbRef,"pokemons"))
-  let pokemons = Array.from(new Map(Object.entries(data.val())).values())
-  return pokemons.map(p => p.sprite_url === '' ? {...p, sprite_url: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/0.png'} : p)
-}
