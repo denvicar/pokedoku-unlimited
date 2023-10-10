16e781bccfcf1937e25f98446021511b7e7e5598
denvicar <30330046+denvicar@users.noreply.github.com>
2023-10-10 19:47:02 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
     },
     output: "export",
     basePath:"/pokedoku-unlimited",
-    assetPrefix:"/pokedoku-unlimited/"
+    assetPrefix:"/pokedoku-unlimited"
 }
 
 module.exports = nextConfig
