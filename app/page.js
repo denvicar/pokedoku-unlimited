d797ed87f7f05b4d0cb8bc74f6583bfc4e5f933f
Ciro Faenza <cfaenza7@gmail.com>
2023-10-14 22:20:39 +0200

    Using local database instead of firebase

--- app/page.js
+++ app/page.js
@@ -1,16 +1,19 @@
 import getData from './lib/db'
 import Schema from "@/app/components/schema";
 import './home.css'
+import {promises as fs} from 'fs'
 
 
 export default async function Home() {
-  const pokemons = await getData()
+    const file = await fs.readFile(process.cwd() + '/app/db.json', 'utf8')
+    const json = JSON.parse(file)
+    const pokemons = Array.from(new Map(Object.entries(json['data']['pokemons'])).values())
 
-  return (
-    <main className={"bg-slate-800"}>
-        <Schema pokemons={pokemons} />
-    </main>
-  )
+    return (
+        <main className={"bg-slate-800"}>
+            <Schema pokemons={pokemons}/>
+        </main>
+    )
 }
 
 
--- app/typequiz/page.js
+++ app/typequiz/page.js
@@ -1,8 +1,11 @@
 import getData from "@/app/lib/db";
 import TypeQuiz from "@/app/components/typeQuiz";
+import {promises as fs} from "fs";
 
 export default async function TypeQuizHome() {
-    const pokemons = await getData()
+    const file = await fs.readFile(process.cwd() + '/app/db.json', 'utf8')
+    const json = JSON.parse(file)
+    const pokemons = Array.from(new Map(Object.entries(json['data']['pokemons'])).values())
 
 
     return <main className={"bg-slate-800"}>
