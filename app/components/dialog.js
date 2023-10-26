9dcdea156bc8ae5607f9bd67597140fbcc36ed61
denvicar <30330046+denvicar@users.noreply.github.com>
2023-10-26 17:45:28 +0200

    Graphical fixes

--- app/components/dialog.js
+++ app/components/dialog.js
@@ -3,7 +3,7 @@ import Search from "@/app/components/search";
 export default function Dialog({children,show, handleClick}) {
     return <>
         <div hidden={!show}
-             className={"min-h-[14%] absolute rounded-lg pb-5 top-20 left-[5%] bg-slate-800 w-[90%] z-20 pt-5"}>
+             className={"min-h-[14%] absolute rounded-lg pb-5 top-20 left-[5%] dark:bg-slate-900 bg-slate-200 w-[90%] z-20 pt-5"}>
             {children}
         </div>
         <div hidden={!show} onClick={handleClick}
